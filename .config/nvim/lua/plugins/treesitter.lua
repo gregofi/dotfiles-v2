38ff8466bbc33be5c47bede49dba38f70063d79e
Filip Gregor <filip.gregor98@gmail.com>
2026-04-12 20:47:28 +0200

    nvim12 compatibility

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,27 +1,28 @@
 local Plugin = {'nvim-treesitter/nvim-treesitter'}
 
-Plugin.main = 'nvim-treesitter.configs'
+Plugin.main = 'nvim-treesitter'
 
-Plugin.version = "^0.9.2"
+Plugin.version = "^10.0.0"
 
 Plugin.dependencies = {
   {'nvim-treesitter/nvim-treesitter-textobjects'}
 }
 
+Plugin.init = function()
+  vim.api.nvim_create_autocmd('FileType', { 
+    callback = function() 
+      -- Enable treesitter highlighting and disable regex syntax
+      pcall(vim.treesitter.start) 
+      -- Enable treesitter-based indentation
+      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()" 
+    end, 
+  }) 
+end
+
 Plugin.opts = {
     auto_install = true,
     ensure_installed = { "cpp", "javascript", "typescript",
   	                     "c", "lua", "vim", "vimdoc", "go" },
-    highlight = {
-        enable = true,
-        disable = function(lang, buf)
-            -- There is probably bug with HTML parser causing nvim to crash silently.
-            if lang == "html" then
-                return true
-            end
-            return false
-        end
-    },
 }
 
 return Plugin
